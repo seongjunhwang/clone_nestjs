d6192eff089828c7d7d7f340ad9fa011306e9ea7
seongjunhwang <samkukzi3@gmail.com>
2020-12-13 22:47:05 +0900

    REST API Practice 2

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Delete, Put, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Put,
+  Body,
+  Param,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movie')
 export class MovieController {
@@ -7,13 +16,19 @@ export class MovieController {
     return 'this will return all movies';
   }
 
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `i will search this after ${searchingYear}`;
+  }
+
   @Get('/:id')
   getOne(@Param('id') movieId: string) {
     return `this will return one movie : ${movieId}`;
   }
 
   @Post()
-  create() {
+  create(@Body() movieData) {
+    console.log(movieData);
     return 'this will create moive';
   }
 
@@ -23,7 +38,10 @@ export class MovieController {
   }
 
   @Put('/:id')
-  update(@Param('id') movieId: string) {
-    return `this will update : ${movieId}`;
+  update(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updateMovie: movieId,
+      updateData,
+    };
   }
 }
