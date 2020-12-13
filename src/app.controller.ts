e5fb360d0984df6202778e8105153286ceb88c0f
seongjunhwang <samkukzi3@gmail.com>
2020-12-13 22:14:15 +0900

    practice nestjs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/hello')
+  sayHello(): string {
+    return this.appService.getHi();
+  }
+  
 }
